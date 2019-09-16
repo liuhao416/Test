a5efc2d4c426e6e4274e4e1c202151c6820950fc
liuhao416 <1916757209@qq.com>
2019-09-16 19:00:32 +0800

    刘皓

---
+++ src/main/scala/scala.scala
@@ -0,0 +1,15 @@
+/**
+  * Description：xxxx<br/>
+  * Copyright (c) ， 2018， Jansonxu <br/>
+  * This program is protected by copyright laws. <br/>
+  * Date： 2019年09月16日
+  *
+  * @author 刘皓
+  * @version : 1.0
+  */
+object scala {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
